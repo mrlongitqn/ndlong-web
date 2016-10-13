1b08af0d0d3db73269c3894dc449ed4d7edcea53
Win_64-PC\Win_64 <thanhdongqn94@gmail.com>
2016-10-13 10:03:20 +0700

    1
    1

---
+++ configs/config.php
@@ -0,0 +1,11 @@
+<?php
+
+define('DB_HOST', 'http://45.32.75.199/');
+define('DB_USER', 'mobi_casher');
+define('DB_PASSWORD', '@mobicasher');
+define('DB_NAME', 'mobi_casher');
+define('BASE_URL', 'http://localhost/ndlong-web/');
+define('SITE_URL', 'http://localhost/ndlong-web/index.php');
+define('ADMIN_DOMAIN', 'http://localhost/ndlong-web/administrator/');
+// Defined APP_PATH
+define('APP_PATH', 'C:/xampp/htdocs/myshopp/');
